3f641775e4f6318e5ad531ecf8daf99165c8b10e
rahul pandey <rahul.p@antino.com>
2024-06-16 15:02:30 +0530

    newchanges

--- src/Component/User/User.js
+++ src/Component/User/User.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./User.css";
 import ChatUser from "./ChatUsers/ChatUser";
 import data from "../../data";
